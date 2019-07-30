ddef4d4c9b1e0bdd667ead8e1bdd456f2d74202c
amably <alex.mably@gmail.com>
2019-07-30 16:37:59 -0500

    add dataset formatting

--- pipelines/Sloth_unsupervised_pipeline.py
+++ pipelines/Sloth_unsupervised_pipeline.py
@@ -13,30 +13,37 @@ step_0.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_re
 step_0.add_output('produce')
 pipeline_description.add_step(step_0)
 
-# Step 1: DISTIL/NK Storc primitive -> unsupervised clustering of records with a label (number of clusters set for kmeans algorithm)
-step_1 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.clustering.k_means.Sloth'))
+# Step 1: dataset_to_dataframe
+step_1 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.dataset_to_dataframe.Common'))
 step_1.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
-step_1.add_hyperparameter(name='nclusters', argument_type= ArgumentType.VALUE, data=2)
-step_1.add_hyperparameter(name='long_format', argument_type= ArgumentType.VALUE, data=True)
+step_1.add_hyperparameter(name='dataframe_resource', argument_type= ArgumentType.VALUE, data='learningData')
 step_1.add_output('produce')
-step_1.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
 pipeline_description.add_step(step_1)
 
-# Step 2 column parser -> labeled semantic types to data types
-step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.column_parser.DataFrameCommon'))
+# Step 2: DISTIL/NK Storc primitive -> unsupervised clustering of records with a label (number of clusters set for kmeans algorithm)
+step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.clustering.k_means.Sloth'))
 step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+step_2.add_hyperparameter(name='nclusters', argument_type= ArgumentType.VALUE, data=2)
+step_2.add_hyperparameter(name='long_format', argument_type= ArgumentType.VALUE, data=True)
 step_2.add_output('produce')
-pipeline_description.add_step(step_2)
+step_2.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+pipeline_description.add_step(step_1)
 
-# Step 5: construct predictions dataframe in proper format
-step_3 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.construct_predictions.DataFrameCommon'))
+# Step 3 column parser -> labeled semantic types to data types
+step_3 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.column_parser.DataFrameCommon'))
 step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
-step_3.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
 step_3.add_output('produce')
 pipeline_description.add_step(step_3)
 
+# Step 4: construct predictions dataframe in proper format
+step_4 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.construct_predictions.DataFrameCommon'))
+step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
+step_4.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
+step_4.add_output('produce')
+pipeline_description.add_step(step_4)
+
 # Final Output
-pipeline_description.add_output(name='output predictions', data_reference='steps.3.produce')
+pipeline_description.add_output(name='output predictions', data_reference='steps.4.produce')
 
 # Output json pipeline
 blob = pipeline_description.to_json()
