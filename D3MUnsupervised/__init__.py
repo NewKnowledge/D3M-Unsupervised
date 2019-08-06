a483c0a88adb452fd6b64b2e784a7df8b79e877f
amably <alex.mably@gmail.com>
2019-08-06 13:23:59 -0500

    change __init__

--- D3MUnsupervised/__init__.py
+++ D3MUnsupervised/__init__.py
@@ -1,5 +1,6 @@
 from D3MUnsupervised.Storc import Storc
 from D3MUnsupervised.Hdbscan import Hdbscan
+from D3MUnsupervised.Tsne import Tsne
 from D3MUnsupervised.timeseries_formatter import TimeSeriesFormatterPrimitive
 
 __version__ = '1.0.0'
@@ -7,5 +8,6 @@ __version__ = '1.0.0'
 __all__ = [
            "Storc",
            "Hdbscan",
+           "Tsne",
            "timeseries_formatter",
            ]
