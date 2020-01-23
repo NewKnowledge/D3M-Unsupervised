990230e5cc1913d7223ce2454588acf866496be5
jgleason <jeffrey.gleason@newknowledge.io>
2020-01-23 16:33:23 -0600

    updates to pipelines, scripts, repository structure for winter 2020 eval

--- pipelines/winter_2020/Hdbscan_pipeline.py
+++ pipelines/winter_2020/Hdbscan_pipeline.py
@@ -7,22 +7,45 @@ import sys
 pipeline_description = Pipeline()
 pipeline_description.add_input(name='inputs')
 
-# Step 0: Denormalize primitive -> put all resources in one dataframe
-step_0 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.denormalize.Common'))
+# # Step 0: Denormalize primitive -> put all resources in one dataframe
+# step_0 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.denormalize.Common'))
+# step_0.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.0')
+# step_0.add_output('produce')
+# pipeline_description.add_step(step_0)
+
+# Step 0: dataset_to_dataframe
+step_0 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.dataset_to_dataframe.Common'))
 step_0.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.0')
 step_0.add_output('produce')
 pipeline_description.add_step(step_0)
 
-# Step 1: dataset_to_dataframe
-step_1 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.dataset_to_dataframe.Common'))
-step_1.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
-step_1.add_output('produce')
+# Step 1: Simple Profiler Column Role Annotation
+step_1 = PrimitiveStep(
+    primitive=index.get_primitive("d3m.primitives.schema_discovery.profiler.Common")
+)
+step_1.add_argument(
+    name="inputs",
+    argument_type=ArgumentType.CONTAINER,
+    data_reference="steps.0.produce",
+)
+step_1.add_output("produce")
 pipeline_description.add_step(step_1)
 
 # Step 2 column parser -> labeled semantic types to data types
 step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.column_parser.Common'))
 step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
 step_2.add_output('produce')
+step_2.add_hyperparameter(
+    name="parse_semantic_types",
+    argument_type=ArgumentType.VALUE,
+    data=[
+        "http://schema.org/Boolean",
+        "http://schema.org/Integer",
+        "http://schema.org/Float",
+        "https://metadata.datadrivendiscovery.org/types/FloatVector",
+        "http://schema.org/DateTime",
+    ],
+)
 pipeline_description.add_step(step_2)
 
 # Step 3: imputer -> imputes null values based on mean of column
@@ -42,6 +65,11 @@ pipeline_description.add_step(step_4)
 # Step 5: extract feature columns
 step_5 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.extract_columns_by_semantic_types.Common'))
 step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.4.produce')
+step_5.add_hyperparameter(
+    name="semantic_types",
+    argument_type=ArgumentType.VALUE,
+    data=["https://metadata.datadrivendiscovery.org/types/Attribute"],
+)
 step_5.add_output('produce')
 pipeline_description.add_step(step_5)
 
